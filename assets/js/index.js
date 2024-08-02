2aaaa3272ce8f7b7df41e445346ffddcd573ebdd
Dev avology <devavology12@gmail.com>
2024-08-02 12:19:47 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,32 +13,32 @@ $(document).ready(function () {
 
 
 
-// Dropdown JS Start
-jQuery(document).ready(function (e) {
-  function t(t) {
-      e(t).bind("click", function (t) {
-          t.preventDefault();
-          e(this).parent().fadeOut()
-      })
-  }
-  e(".dropdown-toggle").click(function () {
-      var t = e(this).parents(".button-dropdown").children(".dropdown").is(":hidden");
-      e(".button-dropdown .dropdown").hide();
-      e(".button-dropdown .dropdown-toggle").removeClass("dropping");
-      if (t) {
-          e(this).parents(".button-dropdown").children(".dropdown").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("dropping")
-      }
-  });
-  e(document).bind("click", function (t) {
-      var n = e(t.target);
-      if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown").hide();
-  });
-  e(document).bind("click", function (t) {
-      var n = e(t.target);
-      if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("dropping");
-  })
-});
-// Dropdown JS End
+// // Dropdown JS Start
+// jQuery(document).ready(function (e) {
+//   function t(t) {
+//       e(t).bind("click", function (t) {
+//           t.preventDefault();
+//           e(this).parent().fadeOut()
+//       })
+//   }
+//   e(".dropdown-toggle").click(function () {
+//       var t = e(this).parents(".button-dropdown").children(".dropdown").is(":hidden");
+//       e(".button-dropdown .dropdown").hide();
+//       e(".button-dropdown .dropdown-toggle").removeClass("dropping");
+//       if (t) {
+//           e(this).parents(".button-dropdown").children(".dropdown").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("dropping")
+//       }
+//   });
+//   e(document).bind("click", function (t) {
+//       var n = e(t.target);
+//       if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown").hide();
+//   });
+//   e(document).bind("click", function (t) {
+//       var n = e(t.target);
+//       if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("dropping");
+//   })
+// });
+// // Dropdown JS End
 
 
 $(document).ready(function(){
@@ -50,3 +50,9 @@ $(document).ready(function(){
 
 
 AOS.init();
+
+$(document).ready(function(){
+  $(".button-dropdown").click(function(){
+    $(this).find(".dropdown").slideToggle();
+  });
+});
